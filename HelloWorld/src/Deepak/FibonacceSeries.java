8884ea3cd808c1759ea372d931ff0ee4217defcc
Deepak <Deepak@iphone>
2023-08-14 18:23:01 +0530

    second commit

--- HelloWorld/src/Deepak/FibonacceSeries.java
+++ HelloWorld/src/Deepak/FibonacceSeries.java
@@ -4,6 +4,16 @@ public class FibonacceSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n1=0,n2=1;
+	    int n3;
+		System.out.print(n1+" "+n2);
+		for(int i=0;i<10;i++) {
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+		}
+		
 
 	}
 
